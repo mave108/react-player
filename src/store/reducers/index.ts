eda6fe88607fba54763aa65af7cc7d5755910cfa
Rakesh <rakeshkumar8900@gmail.com>
2022-08-14 23:51:16 +0530

    Update index.ts

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,12 @@
-import { initialState } from '../initials-state';
+import { initialState, InitialStateTypes } from '../initials-state';
+import * as actionTypes from '../types';
 
-export function playerReducer(state = initialState, action: { type: any }) {
+export function playerReducer(state = initialState, action: { type: any; payload: any }) {
   switch (action.type) {
+    case actionTypes.LOADING:
+      return { ...state, loading: action.payload };
+    case actionTypes.SET_DURATION:
+      return { ...state, duration: action.payload };
     default:
       return state;
   }
