9217bba5ee95c8b843531b703d5ab73348f268f4
Rakesh <rakeshkumar8900@gmail.com>
2022-08-16 00:50:34 +0530

    default set of controls

--- src/components/control-bar/internals.tsx
+++ src/components/control-bar/internals.tsx
@@ -1,6 +1,17 @@
 import React, { FC } from 'react';
 import { ControlBarProps } from './types';
+import { PlayToggle } from '../play-toggle';
+import { TimeDisplay } from '../time-display';
 
 export const ControlBar: FC<ControlBarProps> = ({ children }) => {
   return <div className="control-bar">{children}</div>;
+};
+
+export const DefaultControls = () => {
+  return (
+    <ControlBar>
+      <PlayToggle />
+      <TimeDisplay />
+    </ControlBar>
+  );
 };
