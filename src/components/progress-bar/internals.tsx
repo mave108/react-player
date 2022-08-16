adf7b10fe1e320bddb6b849235d24405f8f94153
Rakesh <rakeshkumar8900@gmail.com>
2022-08-17 00:24:28 +0530

    seekable knob implentation half done

--- src/components/progress-bar/internals.tsx
+++ src/components/progress-bar/internals.tsx
@@ -1,17 +1,63 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useEffect, MouseEventHandler } from 'react';
 import { ProgressBarProps } from './types';
 import './style.scss';
 import { useStore } from '../../store/store';
 
-export const ProgressBar: FC<ProgressBarProps> = ({ progress: progressProps }) => {
+export const ProgressBar: FC<ProgressBarProps> = ({ progress: progressProps, seekable = true }) => {
   const {
-    state: { elapsedTime, duration },
+    state: { elapsedTime, duration, width: videoWidth },
   } = useStore();
-
   const progress = (elapsedTime / duration) * 100;
+  const seekableHandle = useRef<HTMLSpanElement>(null);
+  const meterRef = useRef<HTMLDivElement>(null);
+  const width = 701;
+  const knobWidth = 12;
+
+  function moveKnob(x) {
+    const { current } = seekableHandle;
+    if (current && x <= width && x > 0) {
+      current.style.transform = 'translate(' + x + 'px,-4px)';
+    }
+  }
+
+  function handleSeek(e: React.MouseEvent<HTMLSpanElement>) {
+    const { current } = seekableHandle;
+    if (current) {
+      current.classList.add('keep-visible');
+    }
+    moveKnob(e.clientX);
+  }
+  function mouseUp() {
+    const { current } = seekableHandle;
+    if (current) {
+      setTimeout(() => {
+        current.classList.remove('keep-visible');
+      }, 1000);
+    }
+    window.removeEventListener('mousemove', handleSeek, true);
+  }
+
+  function mouseDown() {
+    window.addEventListener('mousemove', handleSeek, true);
+  }
+  useEffect(() => {
+    const { current } = seekableHandle;
+    if (current) {
+      current.addEventListener('mousedown', mouseDown, false);
+      window.addEventListener('mouseup', mouseUp, false);
+    }
+  });
+
+  useEffect(() => {
+    const onePercent = width / 100;
+    moveKnob(onePercent * progress - knobWidth);
+  }, [progress]);
+
   return (
-    <div className="meter">
-      <span style={{ width: `${progress}%` }}></span>
+    <div className="meter" ref={meterRef}>
+      <span style={{ width: `${progress}%` }}>
+        <span className="knob" ref={seekableHandle} draggable={false}></span>
+      </span>
     </div>
   );
 };
